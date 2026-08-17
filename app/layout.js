185fa15e10222e1b4f726f0b0a62ff905afb6a23
prasadby <prasad.by@gmail.com>
2026-08-17 12:30:11 +0530

    Add Vercel speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Global Market Watch",
